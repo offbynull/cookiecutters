7cccd2dc1f85f54bc4acc5b7edcf6c222c2f305d
Kasra Faghihi <offbynull@users.noreply.github.com>
2022-01-14 18:26:37 -0500

    Fix configure.sh

--- cpp_template/{{cookiecutter.project_name}}/configure.sh
+++ cpp_template/{{cookiecutter.project_name}}/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd build
-conan install .. --build=missing --profile={{cookiecutter.project_name}} -s compiler.libcxx=libstdc++20
+CC={{cookiecutter.cc}} CXX={{cookiecutter.cxx}} conan install .. --build=missing --profile={{cookiecutter.project_name}} -s compiler.libcxx=libstdc++11
 cmake --configure ..
 cd ..
