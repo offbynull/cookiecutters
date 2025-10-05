031d34851495c20e161fbe782289be3fd444a2dc
offbynull <offbynull@gmail.com>
2025-10-04 22:35:27 -0400

    Removed old

--- cpp_template/{{cookiecutter.project_name}}/src/main/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <Poco/ASCIIEncoding.h>
-#include "main.hpp"
-
-int main() {
-    std::cout << "helloworld" << std::endl << Poco::ASCIIEncoding().canonicalName();
-    return 0;
-}
